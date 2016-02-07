36cba944a0a4a70cf884f5e59881fc7ce24544ec
PalermoD <palermo.deschamps@gmail.com>
2016-02-07 14:13:04 -0500

    wrote integration test for home page link

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+
+  test "layout links" do 
+  	get root_path
+  	assert_template 'static_pages/home'
+  	assert_select "a[href=?]", root_path, count: 1
+  end
+end
