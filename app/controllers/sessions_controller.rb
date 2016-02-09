5e7db2d0046e2c9b0dc8eb526024a4af453f7dcc
PalermoD <palermo.deschamps@gmail.com>
2016-02-09 18:02:52 -0500

    some changes to users and sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
-      redirect_to user
+      redirect_back_or user
     else
       # Create an error message.
       flash.now[:danger] = 'Invalid email/password combination'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,8 +12,23 @@ module SessionsHelper
     end
 
     def log_out
-      session.delete(:user_id)
-      @current_user = nil
+        session.delete(:user_id)
+        @current_user = nil
+    end
+
+    def current_user?(user)
+    	user == current_user
+    end 
+
+    # Redirects to stored location (or to the default).
+    def redirect_back_or(default)
+        redirect_to(session[:forwarding_url] || default)
+        session.delete(:forwarding_url)
+    end
+
+    # Stores the URL trying to be accessed.
+    def store_location
+        session[:forwarding_url] = request.url if request.get?
     end
 
 end
