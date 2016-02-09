2273874c907789a8ef8eea2b57a25baace820280
PalermoD <palermo.deschamps@gmail.com>
2016-02-09 14:07:58 -0500

    added profile location and bio to update user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   
   def show
    @user = User.find(params[:id]) 
@@ -15,10 +18,25 @@ class UsersController < ApplicationController
      if @user.save
      	flash[:success] = "Welcome to Votester!"
        log_in @user
-       redirect_to @user 
+       render 'edit' 
      else 
         render 'new'
      end 
+  end 
+
+  def edit
+      @user = User.find(params[:id]) 
+  end
+
+  def update
+  	  @user = User.find(params[:id])
+  	  if @user.update_attributes(user_edit_params)
+  	  	# handle succesful update
+  	  	flash[:success] = "Profile updated"
+        redirect_to @user
+  	  else 
+  	  	render 'edit'
+  	  end 
   end 
 
   private 
@@ -26,6 +44,22 @@ class UsersController < ApplicationController
     def user_params 
     	params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end 
+
+    def user_edit_params 
+    	params.require(:user).permit(:name, :email, :location, :bio)
+    end 
+
+    def logged_in_user
+    	unless logged_in?
+    		flash[:danger] = "Please log in"
+    		redirect_to login_url 
+    	end
+    end 
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207223850) do
+ActiveRecord::Schema.define(version: 20160209180802) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20160207223850) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "location"
+    t.text     "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
