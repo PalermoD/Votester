d2aed675102a420a7b11afa45cc7436e2aa586d1
PalermoD <palermo.deschamps@gmail.com>
2016-02-10 22:07:46 -0500

    vote buttons

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,7 @@ class StaticPagesController < ApplicationController
   def home
   	@user = User.new
   	@feed_items = @user.feed.paginate(page: params[:page])
+  	 
     if logged_in?
       @post  = current_user.posts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
