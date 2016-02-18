b522a68bc269cabe14013a43879ea84dcec34d61
PalermoD <palermo.deschamps@gmail.com>
2016-02-17 19:18:09 -0500

    pagnation limit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
   	@user = User.new
-  	@feed_items = @user.feed.paginate(page: params[:page])
+  	@feed_items = @user.feed.paginate(page: params[:page], :per_page => 10)
     
 
   	 
     if logged_in?
       @post  = current_user.posts.build
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 10)
       
 
     end
