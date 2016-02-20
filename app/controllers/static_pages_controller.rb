7f447fd79a873e680542cff518f997f1727a96a5
PalermoD <palermo.deschamps@gmail.com>
2016-02-20 13:35:45 -0500

    format

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,11 +5,9 @@ class StaticPagesController < ApplicationController
     
 
   	 
-    if logged_in?
-      @post  = current_user.posts.build
-      @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 10)
-      
-
+  if logged_in?
+    @post  = current_user.posts.build
+    @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 10)
     end
   end
 
